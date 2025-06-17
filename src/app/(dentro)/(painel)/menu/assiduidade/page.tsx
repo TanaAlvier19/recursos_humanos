21bf9d3c39975ccf7f136fccc8cacbc45f0a5887
TanaAlvier19 <tanaalvier@gmail.com>
2025-06-17 12:22:08 +0100

    Update page.tsx

--- src/app/(dentro)/(painel)/menu/assiduidade/page.tsx
+++ src/app/(dentro)/(painel)/menu/assiduidade/page.tsx
@@ -101,7 +101,7 @@ try {
   const blob = await (await fetch(imageData)).blob(); // converte base64 para blob
   formData.append("image", blob, "face.jpg");
 
-  const response = await fetch('https://backend-django-2-7qpl.onrender.com/api/register_face/', {
+  const response = await fetch('https://8d3e-102-214-36-231.ngrok-free.app/api/register_face/', {
     method: 'POST',
     headers: {
       'Authorization': `Bearer ${accessToken}`,
