90cdee1b38f0e080c3c9e7aa5655604e09820c8e
TanaAlvier19 <tanaalvier@gmail.com>
2025-06-20 13:17:00 +0100

    ola

--- src/app/(dentro)/(dashboard)/list/assiduidade/page.tsx
+++ src/app/(dentro)/(dashboard)/list/assiduidade/page.tsx
@@ -63,7 +63,7 @@ useEffect(() => {
       },
       {
         enableHighAccuracy: true, 
-        timeout: 5000,            
+        timeout: 10000,            
         maximumAge: 0             
       }
     );
