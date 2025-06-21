898af5912cef841db13db5dab26581ff77820dca
TanaAlvier19 <tanaalvier@gmail.com>
2025-06-21 16:15:07 +0100

    ola

--- src/components/Menu1.tsx
+++ src/components/Menu1.tsx
@@ -1,32 +1,34 @@
 import Image from "next/image";
 import Link from "next/link";
+import { HouseIcon, CalendarCheck, Users,ListChecks, LibraryBig, ChartBarBig ,Info, Settings, LogOut} from "lucide-react";
 
 const menuItems1 = [
   {
     title: "MENU",
     items: [
       {
-        icon: "/home.png",
+        icon: <HouseIcon />,
         label: "Home",
         href: "/funcionarios",
       },
       {
-        icon: "/attendance.png",
+        icon: <CalendarCheck />,
+        label: "Pedir Dispensas",
+        href: "/menu/pedir_dispensa",
+        
+      },
+      {
+        icon: <ListChecks />,
         label: "Minha Assiduidade",
         href: "/menu/assiduidade",
       },
       {
-        icon: "/lesson.png",
-        label: "Pedir Dispensas",
-        href: "/menu/pedir_dispensa",
-      },
-      {
-        icon: "/treinamento.png",
+        icon: <LibraryBig />,
         label: "Minhas Formações",
         href: "/menu/formacoes",
       },
       {
-        icon: "/diagrama.png",
+        icon: <ChartBarBig />,
         label: "Perfomance",
         href: "/list/Perfomance",
       },
@@ -36,17 +38,17 @@ const menuItems1 = [
     title: "OUTROS",
     items: [
       {
-        icon: "/suporte-tecnico.png",
+        icon: <Info />,
         label: "Suporte",
         href: "/Suporte",
       },
       {
-        icon: "/setting.png",
+        icon: <Settings />,
         label: "Definições",
         href: "/Definições",
       },
       {
-        icon: "/logout.png",
+        icon: <LogOut />,
         label: "Sair",
         href: "/Sair",
       },
@@ -58,19 +60,19 @@ const Menu1 = () => {
   return (
     <div className="mt-4 text-sm">
       {menuItems1.map((i) => (
-        <div className="flex flex-col gap-2" key={i.title}>
-          <span className="hidden lg:block text-gray-400 font-light my-4">
-            {i.title}
-          </span>
+        <div className="flex flex-col gap-2 py-4" key={i.title}>
           {i.items.map((item) => {
-            
-              return (
-                <Link
+            return (
+              <Link
                   href={item.href}
                   key={item.label}
-                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight"
+                  className="flex items-center justify-center lg:justify-start gap-4 text-white py-2 md:px-2 rounded-md hover:bg-lamaSkyLight transition-colors duration-200 hover:text-blue-400"
                 >
-                  <Image src={item.icon} alt="" width={20} height={20} />
+                  {typeof item.icon === "string" ? (
+                    <Image src={item.icon} alt="" width={25} height={25} />
+                  ) : (
+                    item.icon
+                  )}
                   <span className="hidden lg:block">{item.label}</span>
                 </Link>
               );
